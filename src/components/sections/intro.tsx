2a25cefe9dcba27909ed53d63034170b2af6711a
PatriciJo <sebastian.patrici@gmail.com>
2025-04-07 23:15:51 +0200

    style(intro): adjust layout and typography for better responsiveness
    Improve the responsiveness of the intro section by adjusting the layout and typography. This includes modifying grid and flex properties, updating text sizes for different screen sizes, and ensuring consistent spacing. These changes enhance the user experience across various devices.

--- src/components/sections/intro.tsx
+++ src/components/sections/intro.tsx
@@ -10,20 +10,20 @@ export const Intro = () => {
           <Picture />
         </div>
       </div>
-      <div className="flex md:flex-2/3 md:min-h-screen items-center p-4">
-        <div className="flex flex-col ">
-          <h2 id="headline" className=" text-4xl w-fit">
+      <div className="flex md:flex-2/3 md:min-h-screen items-center p-4 w-full">
+        <div className="flex flex-col m-4 md:m-0  w-full">
+          <h2 id="headline" className="text-xl sm:text-3xl md:text-3xl w-fit">
             Hi, my name is
           </h2>
           <Shadow>
-            <h1 className=" text-5xl font-black w-fit">
+            <h1 className="text-3xl  sm:text-4xl md:text-5xl font-black w-fit opacity-80">
               Sebastian Patrici Gil
             </h1>
           </Shadow>
-          <h2 className=" text-4xl  w-fit">
-            I am a self-taught Full Stack Developer based in Hamburg/Germany
+          <h2 className="text-xl sm:text-3xl md:text-4xl  w-fit my-4">
+            I am a self-taught Full Stack Developer based in Germany
           </h2>
-          <div className="flex gap-1">
+          <div className="grid gap-1 grid-cols-2 md:grid-cols-4 w-full">
             <Button>Hire</Button>
             <Button>References</Button>
             <Button>View GitHub</Button>
