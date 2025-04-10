b16fd5921b0d6bd2513c8da24f567d7982e1f056
PatriciJo <sebastian.patrici@gmail.com>
2025-04-10 16:16:13 +0200

    feat(Picture): add interactive 3D rotation based on mouse position
    This commit introduces a dynamic 3D rotation effect to the Picture component that responds to the user's mouse movement. The rotation is calculated based on the relative position of the mouse to the center of the component, creating an interactive and engaging user experience.

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -1,13 +1,49 @@
+"use client";
 import Image from "next/image";
 import Shadow from "./Shadow";
+import { useMousePosition } from "./MouseTracker";
+import { useEffect, useRef, useState } from "react";
+type RotationState = {
+  x: number;
+  y: number;
+};
 
 const Picture: React.FC = () => {
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  const [center, setCenter] = useState<RotationState>({ x: 0, y: 0 });
+  const { mousePosition } = useMousePosition();
+
+  useEffect(() => {
+    if (!cardRef.current) return;
+    const card = cardRef.current;
+    const rect = card.getBoundingClientRect();
+
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+
+    setCenter({
+      x: centerX,
+      y: centerY,
+    });
+  }, []);
+
+  const mouseX = mousePosition.x - center.x;
+  const mouseY = mousePosition.y - center.y;
+
+  const rotation = {
+    x: Math.min(10, Math.max(-7, -mouseY / 20) * 1.2),
+    y: Math.min(10, Math.max(-7, mouseX / 20) * 1.2),
+  };
+
   return (
     <div className={`relative aspect-square flex justify-center`}>
       <div
-        className="top-0 mx-[5%] absolute"
+        className="top-0 left-[4%] mx-[5%] absolute"
         style={{
-          transform: `translateY(4%) translateX(4%)`,
+          transform: `translateY(${rotation.x / 4}px) translateX(${
+            -rotation.y / 4
+          }px)`,
         }}
       >
         <Shadow>
@@ -17,8 +53,9 @@ const Picture: React.FC = () => {
 
       <div
         className={`bottom-0 absolute ml-[3%]   `}
+        ref={cardRef}
         style={{
-          transform: `perspective(1000px) rotateX(4deg) rotateY(4deg)`,
+          transform: `perspective(1000px) rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`,
           transformStyle: "preserve-3d",
           marginRight: "5%",
         }}
